1eac40d85b2998774698cd8c6fb8d9c1a3dc6a20
DhulipudiAchyutSaiKrishna <63388971+DhulipudiAchyutSaiKrishna@users.noreply.github.com>
2021-05-10 17:48:44 +0530

    "Learned about @ViewChild() properity"

--- src/app/cockpit/cockpit.component.ts
+++ src/app/cockpit/cockpit.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, EventEmitter, Output} from '@angular/core';
+import { Component, OnInit, EventEmitter, Output, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-cockpit',
@@ -9,20 +9,24 @@ export class CockpitComponent implements OnInit {
   @Output('srvCreated') serverCreated = new EventEmitter<{serverName: string, serverContent: string}>();
   @Output('bpCreated') blueprintCreated = new EventEmitter<{serverName: string, serverContent: string}>();
   // newServerName = '';
-  newServerContent = '';
+  // newServerContent = '';
+  @ViewChild('serverContentInput') serverContentInput : ElementRef;
   constructor() { }
 
   ngOnInit(): void {
   }
 
   onAddServer(name : HTMLInputElement) {
+    console.log(this.serverContentInput)
     // this.serverCreated.emit({serverName: this.newServerName, serverContent: this.newServerContent});
-    this.serverCreated.emit({serverName: name.value, serverContent: this.newServerContent});
+    // this.serverCreated.emit({serverName: name.value, serverContent: this.newServerContent});
+    this.serverCreated.emit({serverName:name.value,serverContent: this.serverContentInput.nativeElement.value});
   }
 
   onAddBlueprint(name : HTMLInputElement) {
     // this.blueprintCreated.emit({serverName: this.newServerName, serverContent: this.newServerContent});
-    this.blueprintCreated.emit({serverName : name.value, serverContent:this.newServerContent});
+    // this.blueprintCreated.emit({serverName : name.value, serverContent:this.newServerContent});
+    this.blueprintCreated.emit({serverName : name.value, serverContent: this.serverContentInput.nativeElement.value});
   }
 
 }
